981ace30d711be131007db257f70bd3f5b839f4f
tonyNingLi <247105189@qq.com>
2020-08-12 09:45:46 +0800

    222333

--- Springboot_Mybatis/src/main/java/com/example/DemoApplication.java
+++ Springboot_Mybatis/src/main/java/com/example/DemoApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.example.mapper") //扫描的mapper111
+@MapperScan("com.example.mapper") //扫描的mapper111222
 @SpringBootApplication
 public class DemoApplication {
 
